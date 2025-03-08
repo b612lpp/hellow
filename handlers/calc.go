2e1defef3af4f5068670671dcecf151ab59bbfc7
b612lpp <asoinf@gmail.com>
2025-03-08 17:50:56 +0300

    calculating gas API added

--- handlers/calc.go
+++ handlers/calc.go
@@ -7,17 +7,38 @@ import (
 )
 
 type incValues struct {
-	Name int `json:"name"`
+	CurrentPercent int `json:"value"`
+	CurrentCost    int `json:"cost"`
+}
+type toFill struct {
+	LitersToFill int `json:"liters"`
+	CostToFill   int `json:"cost"`
 }
 
 func Calc(w http.ResponseWriter, r *http.Request) {
 	var v incValues
 	q := json.NewDecoder(r.Body)
 	q.Decode(&v)
-	fmt.Fprintln(w, doMath(v.Name))
+	if ifReqValid(r) && v.CurrentPercent > 0 && v.CurrentCost > 0 {
+		fmt.Fprintln(w, doMath(v))
+		json.NewEncoder(w).Encode(doMath(v))
+
+	} else {
+		fmt.Fprintln(w, "Wrong request")
+	}
 
 }
 
-func doMath(a int) int {
-	return a + 10
+func doMath(a incValues) (b toFill) {
+	b.LitersToFill = ((85 - a.CurrentPercent) * 1500) / 100
+	b.CostToFill = b.LitersToFill * a.CurrentCost
+	return b
+}
+
+func ifReqValid(r *http.Request) bool {
+
+	if r.Method == "GET" && r.URL.Path == "/calc/" {
+		return true
+	}
+	return false
 }
--- read.md
+++ read.md
@@ -4,3 +4,6 @@ No concreet functionality just testing features.
 
 Now app calculates and returns incoming integer from json
 curl -H "Content-Type: application/json" -d '{"name":22}' http://localhost:8080/calc/
+
+
+curl -H "Content-Type: application/json" -d '{"value":22, "cost":30}' http://localhost:8080/calc/
